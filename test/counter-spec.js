f259167d1c24906898cd0a49842f63e93e12d826
Edward Gao <egao@sent.com>
2017-02-26 21:58:11 -1000

    tests if second time loads faster

--- test/counter-spec.js
+++ test/counter-spec.js
@@ -4,6 +4,11 @@ const server = require('../server');
 const supertest = require('supertest');
 const agent = supertest.agent(server);
 
+var timer;
+var secs1 = 0;
+var secs2 = 0;
+var toggle = true;
+
 describe('sanity', () => {
 	it('should be true', () => {
 		true.should.be.true;
@@ -14,10 +19,40 @@ describe('cache rulz', () => {
 	describe('GET /', () => {
 		it('should load', done => {
 			agent.get('/')
+				.expect('Content-Type', /html/)
+				.expect(200)
 				.end((err, res) => {
 					if(err) throw err;
 					done();
 				});
+		});
+	});
+	describe('GET /slow', () => {
+		timer = setInterval(() => {
+			if(toggle) secs1++;
+			else secs2++;
+		}, 1000);
+		it('should load slowly at first', done => {
+			agent.get('/slow')
+				.expect('Content-Type', /html/)
+				.expect(200)
+				.end((err, res) => {
+					if(err) throw err;
+					toggle = false;
+					done();
+				});
+		}).timeout(10000);
+		it('should load second time', done => {
+			agent.get('/slow')
+				.end((err, res) => {
+					if(err) throw err;
+					clearTimeout(timer);
+					done();
+				});
+		});
+		it('should be faster second time', done => {
+			(secs1 > secs2).should.be.true;
+			done();
 		});
 	});
 });
